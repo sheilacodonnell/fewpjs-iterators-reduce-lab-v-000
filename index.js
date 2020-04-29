cdeb361c8763da39a56bff33d602fd478417f7ea
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-29 16:45:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 let totalBatteries = batteryBatches.reduce(function (currentTotal, number){
-  return number + total;
+  return number + currentTotal;
 })
